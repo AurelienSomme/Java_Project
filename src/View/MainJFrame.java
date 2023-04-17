fb972a622a2ef700068d1b4557a7c5aa7fa23639
AurelienSomme <102598180+AurelienSomme@users.noreply.github.com>
2023-04-17 19:10:53 +0200

    Adding menuBar
    Adding JMenuBar with JMenuItem

--- src/View/MainJFrame.java
+++ src/View/MainJFrame.java
@@ -1,10 +1,20 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class MainJFrame extends JFrame {
+
+    private Container container;
+
+    private JMenuBar menuBar;
+    private JMenu menuItem, menuSearch, menuApplication;
+
+    private JMenuItem addMenu, deleteMenu, updateMenu, displayMenu;
+    private JMenuItem queryMenu, taskMenu;
+    private JMenuItem quitMenu;
 
     public MainJFrame(){
 
@@ -21,6 +31,41 @@ public class MainJFrame extends JFrame {
         });
 
         setVisible(true);
+
+        container = getContentPane();
+
+        menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        menuItem = new JMenu("Item");
+        menuSearch = new JMenu("Search");
+        menuApplication = new JMenu("Application");
+
+        addMenu = new JMenuItem("Add");
+        deleteMenu = new JMenuItem("Delete");
+        updateMenu = new JMenuItem("Update");
+        displayMenu = new JMenuItem("Display");
+        queryMenu = new JMenuItem("Query");
+        taskMenu = new JMenuItem("Task");
+        quitMenu = new JMenuItem("Quit");
+
+        menuItem.add(addMenu);
+        menuItem.add(deleteMenu);
+        menuItem.add(updateMenu);
+        menuItem.add(displayMenu);
+        menuSearch.add(queryMenu);
+        menuSearch.add(taskMenu);
+        menuApplication.add(quitMenu);
+
+        menuBar.add(menuItem);
+        menuBar.add(menuSearch);
+        menuBar.add(menuApplication);
+
+
+        //Refresh display
+        revalidate();
+
+
 
     }
 
